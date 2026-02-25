c163d1255093b165d2fd1581f7a9927dc04f0fa5
ThaiDuisss <thai110504@gmail.com>
2026-02-25 19:03:27 +0700

    update login add logout fe

--- SBA-BE/src/main/java/com/laundry/dto/response/UserResponse.java
+++ SBA-BE/src/main/java/com/laundry/dto/response/UserResponse.java
@@ -11,7 +11,7 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserResponse {
-    Long userId;
+    Long id;
     String username;
     boolean emailVerified;
     Set<RoleResponse> roles;
