5bcf8c4ba70fa4fd52577604c5f3d2d113f6f455
annguyen <annvhe181169@fpt.edu.vn>
2026-02-25 18:43:40 +0700

    register vs getmyinfor

--- SBA-BE/src/main/java/com/laundry/service/UserService.java
+++ SBA-BE/src/main/java/com/laundry/service/UserService.java
@@ -6,5 +6,9 @@ import com.laundry.dto.response.UserResponse;
 
 public interface UserService  {
     UserResponse register(UserRequest userRegister);
-    ProfileResponse getMyInfo(Long userId);
+    UserResponse getUserById(Long userId);
+    UserResponse getUserByUsername(String username);
+    boolean verifyEmail(String token);
+    void sendEmail(String email);
+    void delete(Long id);
 }
