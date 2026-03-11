a9b087ae959c4fa4c1e24f353ba5b67a30c7c227
daqhuy111 <dqh07082004@gmail.com>
2026-03-12 05:08:11 +0700

    admin manager route challenge

---
+++ SBA-BE/src/main/java/com/running_platform/dto/request/AdminUpdateRouteChallengeRequest.java
@@ -0,0 +1,34 @@
+package com.running_platform.dto.request;
+
+import com.running_platform.enums.ChallengeType;
+import com.running_platform.enums.VisibilityEnum;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AdminUpdateRouteChallengeRequest {
+
+    String title;
+
+    String description;
+
+    ChallengeType type;
+
+    BigDecimal targetValue;
+
+    Instant startTime;
+
+    Instant endTime;
+
+    VisibilityEnum visibility;
+
+    ChallengeType challengeType;
+}
---
+++ SBA-BE/src/main/java/com/running_platform/service/impl/RouteChallengeServiceImpl.java
@@ -0,0 +1,94 @@
+package com.running_platform.service.impl;
+
+import com.running_platform.constant.ErrorEnum;
+import com.running_platform.dto.request.AdminCreateRouteChallengeRequest;
+import com.running_platform.dto.request.AdminUpdateRouteChallengeRequest;
+import com.running_platform.dto.response.AdminRouteChallengeResponse;
+import com.running_platform.dto.response.PageResponse;
+import com.running_platform.entity.RouteChallege.RouteChallenge;
+import com.running_platform.enums.ChallengeStatus;
+import com.running_platform.exception.AppException;
+import com.running_platform.mapper.RouteChallengeMapper;
+import com.running_platform.repository.RouteChallengeRepository;
+import com.running_platform.service.RouteChallengeService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RouteChallengeServiceImpl implements RouteChallengeService {
+
+    RouteChallengeRepository routeChallengeRepository;
+    RouteChallengeMapper routeChallengeMapper;
+
+
+    @Override
+    public AdminRouteChallengeResponse createRouteChallenge(
+            AdminCreateRouteChallengeRequest request
+    ) {
+
+        RouteChallenge challenge = routeChallengeMapper.toEntity(request);
+
+        challenge.setStatus(ChallengeStatus.ACTIVE);
+
+        routeChallengeRepository.save(challenge);
+
+        return routeChallengeMapper.toResponse(challenge);
+    }
+
+    @Override
+    public PageResponse<AdminRouteChallengeResponse> getRouteChallenges(
+            int page,
+            int size
+    ) {
+
+        Page<RouteChallenge> challengePage =
+                routeChallengeRepository.findAll(PageRequest.of(page, size));
+
+        Page<AdminRouteChallengeResponse> dtoPage =
+                challengePage.map(routeChallengeMapper::toResponse);
+
+        return PageResponse.from(dtoPage);
+    }
+
+    @Override
+    public AdminRouteChallengeResponse getRouteChallengeById(Long id) {
+
+        RouteChallenge challenge = routeChallengeRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorEnum.UNKNOWN_ERROR));
+
+        return routeChallengeMapper.toResponse(challenge);
+    }
+
+    @Override
+    public AdminRouteChallengeResponse updateRouteChallenge(
+            Long id,
+            AdminUpdateRouteChallengeRequest request
+    ) {
+
+        RouteChallenge challenge = routeChallengeRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorEnum.UNKNOWN_ERROR));
+
+        routeChallengeMapper.updateEntity(request, challenge);
+
+        routeChallengeRepository.save(challenge);
+
+        return routeChallengeMapper.toResponse(challenge);
+    }
+
+    @Override
+    public void deleteRouteChallenge(Long id) {
+
+        RouteChallenge challenge = routeChallengeRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorEnum.UNKNOWN_ERROR));
+
+        challenge.setIsDeleted(true);
+
+        routeChallengeRepository.save(challenge);
+    }
+}
