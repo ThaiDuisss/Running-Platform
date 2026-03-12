7bcb740e88bfedbb3b5101cc61a4c40fe0121660
daqhuy111 <dqh07082004@gmail.com>
2026-03-12 09:10:51 +0700

    Merge pull request #11 from ThaiDuisss/admin-manage-user
    user management

--- SBA-BE/src/main/java/com/running_platform/service/UserService.java
+++ SBA-BE/src/main/java/com/running_platform/service/UserService.java
@@ -1,7 +1,10 @@
 package com.running_platform.service;
 
+import com.running_platform.dto.request.ResetPasswordRequest;
 import com.running_platform.dto.request.UserRequest;
 import com.running_platform.dto.response.UserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UserService  {
     UserResponse register(UserRequest userRegister);
