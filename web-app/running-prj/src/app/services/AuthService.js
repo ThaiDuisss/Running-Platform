5f543045e0b9da4cbb2d34323e3ef2b660f51b7f
ThaiDuisss <thai110504@gmail.com>
2026-03-12 16:03:55 +0700

    update log in

--- web-app/running-prj/src/app/services/AuthService.js
+++ web-app/running-prj/src/app/services/AuthService.js
@@ -24,8 +24,14 @@ export const authService = {
     },
 
     register: async (payload) => {
-        const res = await axiosClient.post("/auth/register", payload);
-        return res.data;
+        try {
+            const res = await axiosClient.post("/auth/register", payload);
+            return res.data;
+        } catch (error) {
+            console.error("Registration error:", error);
+            throw error;
+        }
+
     },
 
     refreshToken: async () => {
@@ -47,17 +53,6 @@ export const authService = {
         localStorage.removeItem("ACCESS-TOKEN");
         localStorage.removeItem("userInfo");
     },
-    
-    forgotPassword: async (email) => {
-        const res = await axiosClient.post(`/auth/forgot-password?email=${email}`);
-        return res.data;
-    },
-
-    resetPassword: async ({ token, newPassword }) => {
-        const res = await axiosClient.post("/auth/reset-password", { token, newPassword });
-        return res.data;
-    },
-    
 
     signinWithSocialPlatform: async (platform, routeQueryParams) => {
         console.log("Signing in with platform:", platform, routeQueryParams);
@@ -79,4 +74,5 @@ export const authService = {
         }
         window.location.href = oauth2Url;
     }
+
 }
